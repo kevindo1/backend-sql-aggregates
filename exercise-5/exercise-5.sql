5c0fffdd4edb8aa92f84f09dab7546c052bf5dfb
Kevin Do <kevind0501@gmail.com>
2022-03-23 13:02:03 -0700

    passing fifth excercise

--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,1 +1,11 @@
 -- the total run time of all inventory items if they were all played back to back
+
+SELECT
+  SUM(film.length) as sum
+FROM
+  inventory
+LEFT JOIN
+  film
+ON
+  film.film_id = inventory.film_id
+
