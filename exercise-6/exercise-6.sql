2d96fad1c2783450ec66c638b815a2782e82e64d
Kevin Do <kevind0501@gmail.com>
2022-03-24 11:21:10 -0700

    passing sixth excercise

--- exercise-6/exercise-6.sql
+++ exercise-6/exercise-6.sql
@@ -2,3 +2,20 @@
 -- include email and rank
 -- use RANK ORDER BY the SUM of the amount in descending order
 
+SELECT
+  customer.email,
+  RANK() OVER (ORDER BY SUM(payment.amount) DESC)
+FROM
+  payment
+LEFT JOIN
+  rental
+ON 
+  payment.rental_id = rental.rental_id
+LEFT JOIN
+  customer
+ON
+  rental.customer_id = customer.customer_id
+GROUP BY
+  customer.email
+ORDER BY
+  rank 
